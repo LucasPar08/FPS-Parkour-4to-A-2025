60dd66648c4564b6b9774aa91ebd17a336b93f1e
LucasPar08 <lucaspark2008@gmail.com>
2025-05-30 09:01:57 -0300

    clase 30/05

---
+++ Assets/Scripts/DineroManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float playerMoney;
+
+    public void UpdateMoney(float amount)
+    {
+        if (playerMoney + amount < 0)
+        {
+            //impedir compra
+        }
+        else
+        {
+            playerMoney += amount;
+        }
+    }
+}
---
+++ Assets/Scripts/MercanciaScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int precio;
+    public DineroManager dineroManager;
+
+    void Start()
+    {
+        dineroManager = FindObjectOfType<DineroManager>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            dineroManager.UpdateMoney(precio);
+            Destroy(gameObject);
+        }
+    }
+}
